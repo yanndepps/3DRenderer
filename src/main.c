29446a590bbd2787130ce2b4af4f6f9232dcd20f
Yann Depps <yann.deperrot@protonmail.com>
2021-01-11 18:14:54 +0100

    creating an SDL window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+// Global Variables
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error initializing SDL.\n");
+        return false;
+    }
+    // create a SDL window
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
+    // is it a valid pointer and do we have content ?
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    // create a SDL renderer
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    // is it a valid pointer and do we have content ?
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+    // if everything went fine
+    return true;
+}
 
 int main(void) {
-    printf("Hello, World!\n");
+    is_running = initialize_window();
     return 0;
 }
