0e2a14f2f3195a28dafeeda40d2ad6ea1d0c1909
Yann Depps <yann.deperrot@protonmail.com>
2021-01-12 15:33:19 +0100

    color buffer, allocating memory, freeing resources

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
@@ -6,6 +7,10 @@
 bool is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
+// ...
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -17,8 +22,8 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
     // is it a valid pointer and do we have content ?
@@ -40,7 +45,7 @@ bool initialize_window(void) {
 
 // ---
 void setup(void) {
-    // ...
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
 void process_input(void) {
@@ -68,6 +73,14 @@ void render(void) {
     SDL_RenderPresent(renderer);
 }
 
+// --- Freeing ressources
+void destroy_window(void) {
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
 // Main function
 int main(void) {
     is_running = initialize_window();
@@ -77,5 +90,7 @@ int main(void) {
         update();
         render();
     }
+    // free memory
+    destroy_window();
     return 0;
 }
