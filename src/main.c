ca78cf508f815442007b85e59e498ca3a8e1608e
Yann Depps <yann.deperrot@protonmail.com>
2021-01-20 16:31:57 +0100

    SDL texture

--- src/main.c
+++ src/main.c
@@ -5,10 +5,14 @@
 
 // Global Variables
 bool is_running = false;
+
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-// ...
+// dclare a pointer in an array of uint32 elements
 uint32_t* color_buffer = NULL;
+// ...
+SDL_Texture* color_buffer_texture = NULL;
+
 int window_width = 800;
 int window_height = 600;
 
@@ -45,7 +49,17 @@ bool initialize_window(void) {
 
 // ---
 void setup(void) {
+    // allocate the required bytes in memory for the color buffer
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+
+    // create a SDL texture that is used to display the color buffer
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 }
 
 void process_input(void) {
@@ -67,9 +81,29 @@ void update(void) {
     // ...
 }
 
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            color_buffer[( window_width * y ) + x] = color;
+        }
+    }
+}
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
     SDL_RenderPresent(renderer);
 }
 
