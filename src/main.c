076199a62c1bec738d14704f6b7bda0102ad6fd8
Yann Depps <yann.deperrot@protonmail.com>
2021-01-26 14:41:48 +0100

    Dots background

--- src/main.c
+++ src/main.c
@@ -112,6 +112,15 @@ void draw_grid(void) {
     }
 }
 
+// -- Draw small dots instead of lines
+void draw_dots(void) {
+    for (int y = 0; y < window_height; y += 10) {
+        for (int x = 0; x < window_width; x += 10) {
+            color_buffer[( window_width * y ) + x] = 0xFF444444;
+        }
+    }
+}
+
 void clear_color_buffer(uint32_t color) {
     for (int y = 0; y < window_height; y++) {
         for (int x = 0; x < window_width; x++) {
@@ -131,9 +140,10 @@ void render_color_buffer(void) {
 }
 
 void render(void) {
-    /* SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); */
-    /* SDL_RenderClear(renderer); */
-    draw_grid();
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    draw_dots();
+    /* draw_grid(); */
     render_color_buffer();
     clear_color_buffer(0xFF000000);
     SDL_RenderPresent(renderer);
