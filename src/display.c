69e062d2d40cfb912cab181e6bf44549afc3bdfe
yanndepps <yann.deperrot@protonmail.com>
2021-10-12 17:02:28 +0200

    dynamic mesh vertices and faces

--- src/display.c
+++ src/display.c
@@ -139,7 +139,6 @@ void render_color_buffer(void) {
 
 // --- Freeing ressources
 void destroy_window(void) {
-  free(color_buffer);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
